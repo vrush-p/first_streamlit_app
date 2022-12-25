7c808d632ccd662456a72798f051a01176ab9a5c
Vrushali <117810991+vrush-p@users.noreply.github.com>
2022-12-25 21:41:04 +0530

    Function Introduction

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,31 @@ streamlit.dataframe(fruits_to_show)
 
 # Import Requests
 streamlit.header("Fruityvice Fruit Advice!")
+
+# define function get_fruityvice_data
+def get_fruityvice_data
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json()) ##display data on screen as json content
+
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
     # streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) - Moved to function
     #streamlit.text(fruityvice_response.json()) ##display data on screen as json content
 
     # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  - Moved to function
     # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    bank_from_function = get_fruityvice_data(fruit_choice)
+    # streamlit.dataframe(fruityvice_normalized) - return of function to be displayed
+    streamlit.dataframe(bank_from_function)
 except URLError as e:
     streamlit.error()
     
