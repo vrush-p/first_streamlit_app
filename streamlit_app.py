89ebc8cf9e5868bf1dece2ba22e6e4742725b719
Vrushali <117810991+vrush-p@users.noreply.github.com>
2022-12-25 21:51:33 +0530

    another function

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,17 +49,24 @@ try:
     streamlit.dataframe(bank_from_function)
 except URLError as e:
     streamlit.error()
-    
-streamlit.stop()
 
-# connect with snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-## my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()  
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur
+        my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+        return my_cur.fetchall()
+    
+#introduce button action
+if streamlit.button('Get Fruit Load List'):
+    # connect with snowflake
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    #my_cur = my_cnx.cursor() --moved to function get_fruit_load_list()
+    #my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST") -- moved to a function get_fruit_load_list()
+    ## my_data_row = my_cur.fetchone()
+    #my_data_rows = my_cur.fetchall() --moved to function get_fruit_load_list()
+    my_data_row = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
