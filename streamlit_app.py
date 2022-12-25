0ead801de4ed288a6c7b37a83ba355ed528b9144
Vrushali <117810991+vrush-p@users.noreply.github.com>
2022-12-25 21:08:57 +0530

    Error for all data fetch

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = mycur.fetchall() # my_cur.fetchone()
+## my_data_row = my_cur.fetchone()
+my_data_rows = mycur.fetchall()  
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
